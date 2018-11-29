4dced4b1b51fc95d01277e9b650a7cb7e92512d9
ThisIsMyWay <skaczacymuchomor@gmail.com>
2018-11-29 15:32:55 +0100

    update book details with time discount

--- BackendEE/Service/src/main/java/com/playingwithee/service/books/BooksService.java
+++ BackendEE/Service/src/main/java/com/playingwithee/service/books/BooksService.java
@@ -1,7 +1,9 @@
 package com.playingwithee.service.books;
 
 import com.playingwithee.dal.book.api.BookRepo;
+import com.playingwithee.dal.book.api.dto.BookDetailsData;
 import com.playingwithee.dal.book.api.dto.BookOverallData;
+import com.playingwithee.dal.book.api.dto.DiscountDetatilsData;
 import com.playingwithee.service.books.timediscount.TimeDiscount;
 import com.playingwithee.service.books.timediscount.TimeDiscountBean;
 
@@ -23,7 +25,7 @@ public class BooksService implements Serializable {
     @EJB
     private BookRepo bookRepo;
 
-    public Set<BookOverallData> getListOfBooks(){
+    public Set<BookOverallData> getListOfBooks() {
         Set<BookOverallData> booksToReturn = bookRepo.getAllBooks();
 
         updateBooksWithTimeDiscount(booksToReturn);
@@ -31,18 +33,29 @@ public class BooksService implements Serializable {
         return booksToReturn;
     }
 
-    private void updateBooksWithTimeDiscount(Set<BookOverallData> booksToReturn) {
+    public Optional<BookDetailsData> getBookDetails(Long idOfBook) {
+        final Optional<BookDetailsData> retrievedBook = bookRepo.getBookDetailsById(idOfBook);
+
+        retrievedBook.ifPresent(p -> Optional.ofNullable(getAccumulatedPromotions().get(p.getIdOfBook()))
+                .ifPresent(e -> p.getDiscounts().add(new DiscountDetatilsData(null, "Happy Hour Discount", e.getDiscountRate()))));
+
+        return retrievedBook;
+    }
+
+    private Map<Long, TimeDiscount> getAccumulatedPromotions() {
         Set<TimeDiscount> actualPromotions = timeDiscountBean.getActualPromotions();
 
-        Map<Long, TimeDiscount> accumulator =
-                actualPromotions.stream()
-                        .collect(Collectors.toMap(TimeDiscount::getIdOfBook, Function.identity()));
+        return actualPromotions.stream()
+                .collect(Collectors.toMap(TimeDiscount::getIdOfBook, Function.identity()));
+    }
+
+    private void updateBooksWithTimeDiscount(Set<BookOverallData> booksToReturn) {
+        Map<Long, TimeDiscount> accumulator = getAccumulatedPromotions();
 
         booksToReturn.forEach(e ->
                 Optional.ofNullable(accumulator.get(e.getIdOfBook()))
                         .ifPresent(p -> e.setDiscountRate(e.getDiscountRate() + p.getDiscountRate()))
         );
     }
-
 
 }
