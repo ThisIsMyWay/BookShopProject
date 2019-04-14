03c10e217b32b7784a7473a5f961d72ab2d96054
ThisIsMyWay <skaczacymuchomor@gmail.com>
2019-04-14 23:47:57 +0200

    extracted expected responses to files

--- BackendEE/RestApi/src/test/java/com/playingwithee/authors/AuthorsEndpointIT.java
+++ BackendEE/RestApi/src/test/java/com/playingwithee/authors/AuthorsEndpointIT.java
@@ -1,43 +1,19 @@
 package com.playingwithee.authors;
 
 import io.restassured.response.ResponseBody;
+import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Objects;
 
 import static io.restassured.RestAssured.given;
 
 public class AuthorsEndpointIT {
 
-    private static final String authorOfId4ExpectedBody = "{\n" +
-            "    \"links\": [\n" +
-            "        {\n" +
-            "            \"href\": \"http://192.168.99.100:8080/bookshop/rest/authors/10\",\n" +
-            "            \"rel\": \"self\",\n" +
-            "            \"method\": \"GET\"\n" +
-            "        }\n" +
-            "    ],\n" +
-            "    \"authorId\": 10,\n" +
-            "    \"name\": \"Gabriel\",\n" +
-            "    \"surname\": \"García Márquez\",\n" +
-            "    \"birthDate\": \"1927-03-06\",\n" +
-            "    \"deathDate\": \"2014-04-14\",\n" +
-            "    \"booksOfAuthor\": [\n" +
-            "        {\n" +
-            "            \"links\": [\n" +
-            "                {\n" +
-            "                    \"href\": \"http://192.168.99.100:8080/bookshop/rest/books/10\",\n" +
-            "                    \"rel\": \"book_details\",\n" +
-            "                    \"method\": \"GET\"\n" +
-            "                }\n" +
-            "            ],\n" +
-            "            \"id\": 10,\n" +
-            "            \"title\": \"Love in the Time of Cholera\"\n" +
-            "        }\n" +
-            "    ]\n" +
-            "}";
-
     @Test
-    public void shouldReturnAuthorWithCode200() {
+    public void shouldReturnAuthorWithCode200() throws IOException {
 
         final ResponseBody bodyToValidate =
                 given().
@@ -48,6 +24,7 @@ public class AuthorsEndpointIT {
                     statusCode(200).
                     extract().response().getBody();
 
+        final String authorOfId4ExpectedBody =  IOUtils.toString(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("com/playingwithee/authors/ExpectedBodyForShouldReturnAuthorWithCode200.json")),"UTF-8");
         Assert.assertEquals(authorOfId4ExpectedBody, bodyToValidate.prettyPrint());
     }
 
